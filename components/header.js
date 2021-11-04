b5565a7ba6a5b41c96520a03e9fee0bfe72c3a16
ASK-100 <86280212+ASK-100@users.noreply.github.com>
2021-11-04 21:08:47 +0000

    header

--- components/header.js
+++ components/header.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { AntDesign } from '@expo/vector-icons';
+// import { AntDesign } from '@expo/vector-icons';
 
 export default function Header() {
     return(
       <View style={styles.container}>
         <View style={styles.headertxt}>
-          <TouchableOpacity>
+          {/* <TouchableOpacity>
             <AntDesign name='menu-fold' size={35} color="#e1e1e1" style={styles.iconStyle}/>
-          </TouchableOpacity>
+          </TouchableOpacity> */}
           <Text style={styles.title}>SHOP-IN</Text>
         </View>
       </View>
@@ -19,29 +19,27 @@ const styles = StyleSheet.create({
     
   },
   headertxt: {
-    backgroundColor: '#FF7F50',
+    backgroundColor: 'coral',
     alignItems: 'center',
     flexDirection: 'row',
     justifyContent: 'space-evenly',
   },
   navigation: {
-    color: '#40e0d0',
+    color: 'coral',
     alignItems: 'flex-start',
   },
   title: {
-    
     color: '#e1e1e1', 
     textAlign: 'center', 
     fontWeight: 'bold',
     fontSize: 40,
-    padding: 10,
     paddingTop: 25,
-    paddingRight: 100,
-    paddingLeft: 60,
-    marginHorizontal: 10,
+    width: 5006
+
   },
   iconStyle: {
     paddingTop: 20,
   }
     
-  });
+});
+  
