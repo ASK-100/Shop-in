d9d0ce7efdd96850726df8d6b13b5698b48d0980
ASK-100 <86280212+ASK-100@users.noreply.github.com>
2021-11-04 21:03:53 +0000

    twf, login, header

--- App.js
+++ App.js
@@ -1,24 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, } from 'react-native';
+import { StyleSheet, Text, View, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import Header from './components/header';
+import Login from './screens/login';
 
-export default function App() {
+
+export default function App(){
   return (
-    <View style={styles.container}>
-      <StatusBar backgroundColor={'#FF7F50'}/>
-      <Header/>
-    </View>
+    <TouchableWithoutFeedback onPress={() => {Keyboard.dismiss();}}>
+      <View style={styles.container}>
+        <View>
+          <StatusBar backgroundColor={'#bb5d40'}/>
+          <Header />
+          <Login />
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#e1e1e1',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: '#ffff9e',
     alignItems: 'center',
-
-  },
-
-
+    margin: 0,
+    padding: 0,
+    color: '#40e0d0',
+  }
 });
