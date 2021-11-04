4cdd0fac3cfb1d31f7eb34343af1be7dfccb67be
ASK-100 <86280212+ASK-100@users.noreply.github.com>
2021-11-04 21:09:11 +0000

    styles

---
+++ components/styles.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+import {View, Text, Image} from 'react-native';
+import Constants from 'expo-constants';
+
+const StatusBarHeight = Constants.StatusBarHeight;
+
+export const Colors ={
+  primary: 'silver',
+  secondary: '#ff7f50',
+  tertiary: '#adf802',
+  darklight: '#9ca3af',
+  brand: '#6d28d9',
+  green: '#10b3981',
+  red: '#ef4444',
+};
+
+const {primary, secondary, tertiary, darklight, brand, green, red} = Colors;
+
+export const StyledContainer = styled.View`
+  flex: 1;
+  padding: 25px;
+  padding-top: ${StatusBarHeight + 10}px;
+  background-color: ${primary};
+`
+
+export const InnerContainer = styled.View`
+  flex: 1;
+  width: 100%;
+  align-items: center;
+`;
+
+export const PageLogo = styled.Image`
+  width: 250px;
+  height: 200px;
+`;
+
+export const PageTitle = styled.Text`
+  font-size: 30px;
+  text-align: center;
+  font-weight: bold;
+  color: ${brand};
+  padding: 10px;
+`
